c28e366af999752e84478ba460b5e05eb8e84ac4
RoboPhred <robophred@gmail.com>
2021-12-04 13:29:00 -0700

    Fix zip file package id

--- Crucible.Core/CruciblePackages/CruciblePackageMod.cs
+++ Crucible.Core/CruciblePackages/CruciblePackageMod.cs
@@ -88,7 +88,8 @@ namespace RoboPhredDev.PotionCraft.Crucible.CruciblePackages
         {
             get
             {
-                return this.ID.Replace(" ", "_").ToLowerInvariant();
+                /* return this.ID.Replace(" ", "_").ToLowerInvariant(); */
+                return this.ID;
             }
         }
 
@@ -130,10 +131,11 @@ namespace RoboPhredDev.PotionCraft.Crucible.CruciblePackages
         /// <returns>A <see cref="CruciblePackageMod"/> loaded from the file.</returns>
         public static CruciblePackageMod LoadFromZip(string zipFilePath)
         {
+            UnityEngine.Debug.Log($"Loading mod from zip file {new FileInfo(zipFilePath).Name}");
             loadingNodes = new List<CruciblePackageConfigNode>();
             try
             {
-                var mod = new CruciblePackageMod(new FileInfo(zipFilePath).Name, new ZipResourceProvider(zipFilePath));
+                var mod = new CruciblePackageMod(Path.GetFileNameWithoutExtension(zipFilePath), new ZipResourceProvider(zipFilePath));
                 mod.parsedData = CrucibleResources.WithResourceProvider(mod, () => Deserializer.DeserializeFromResource<CruciblePackageModConfig>("package.yml"));
                 loadingNodes.ForEach(x => x.SetParentMod(mod));
                 return mod;
